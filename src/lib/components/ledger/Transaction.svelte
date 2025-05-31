9ed3f4f539d917ac506c3cc1cb983c062341d28d
Ángel Díaz <ad9311@proton.me>
2025-05-31 10:47:25 -0500

    Enhance Transaction and TransactionList components with action handling and improved display
    This commit updates the Transaction component to include action buttons for editing and deleting transactions, enhancing user interaction. The TransactionList component is introduced to display a list of transactions with links to view details, improving the overall organization and usability of the ledger interface. Additionally, the layout and structure of both components are refined for better clarity and maintainability.

--- src/lib/components/ledger/Transaction.svelte
+++ src/lib/components/ledger/Transaction.svelte
@@ -1,5 +1,41 @@
-<!-- <script lang="ts">
+<script lang="ts">
 	import type { Transaction } from '$lib/server/db/schema';
+	import type { Action } from '$lib/shared';
+	import Amount from '../Amount.svelte';
 
 	export let transaction: Transaction;
-</script> -->
+	export let ledgerType: 'budget' | 'payable/receivable';
+
+	const type = transaction.type === 'credit' ? 'credit' : 'debit';
+	const ledgerPath = ledgerType === 'budget' ? 'budgets' : 'accounts';
+	const actions: Action[] = [
+		{
+			label: 'Edit',
+			href: `/ledgers/${ledgerPath}/${transaction.ledgerId}/transactions/${transaction.id}/edit`
+		},
+		{
+			label: 'Delete',
+			onClick: () => {
+				// TODO: Implement delete transaction
+			}
+		}
+	];
+</script>
+
+<div>
+	<h2>{transaction.description}</h2>
+	<p>{transaction.type}</p>
+	<p>{transaction.category}</p>
+	<p>{transaction.date}</p>
+	<p>Amount: <Amount value={transaction.amount} {type} /></p>
+	<hr />
+	<div>
+		{#each actions as action (action.label)}
+			{#if action.href}
+				<a href={action.href}>{action.label}</a>
+			{:else}
+				<button onclick={action.onClick}>{action.label}</button>
+			{/if}
+		{/each}
+	</div>
+</div>
---
+++ src/lib/components/ledger/TransactionList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Transaction } from '$lib/server/db/schema';
+	import Amount from '$lib/components/Amount.svelte';
+
+	export let transactions: Transaction[];
+	export let type: 'credit' | 'debit';
+	export let ledgerType: 'budget' | 'payable/receivable';
+
+	const ledgerPath = ledgerType === 'budget' ? 'budgets' : 'accounts';
+</script>
+
+<div>
+	{#each transactions as transaction}
+		<p>{transaction.description}</p>
+		<p><Amount value={transaction.amount} {type} /></p>
+		<a href={`/ledgers/${ledgerPath}/${transaction.ledgerId}/transactions/${transaction.id}`}
+			>View</a
+		>
+	{/each}
+</div>
