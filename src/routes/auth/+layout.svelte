c1e1b5bea59274175eb256e125e083b6c66ecf4e
Ángel Díaz <ad9311@proton.me>
2025-05-31 10:00:31 -0500

    Refactor app.css to update color variables and improve styling consistency; enhance layout in +layout.svelte by applying primary color to the header and streamlining structure for better semantic HTML.

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main class="container mx-auto">
-	<h1 class="my-20 text-center text-3xl font-bold">NineTe</h1>
+	<h1 class="text-primary my-20 text-center text-3xl font-bold">NineTe</h1>
 	<div class="mx-auto max-w-xl">
 		{@render children()}
 	</div>
