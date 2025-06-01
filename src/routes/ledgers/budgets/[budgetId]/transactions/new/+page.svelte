9ea4f7040deed8ce3eb68e5977d345c2fa698334
Ángel Díaz <ad9311@proton.me>
2025-06-01 11:45:39 -0500

    Refactor transaction form layout and enhance styling
    This commit updates the transaction creation form in the Svelte component to utilize a new CSS class structure for improved styling and organization. The form elements are now wrapped in divs with the class "form-group" for better spacing and layout. Additionally, a "form-actions" class is added to the submit button container, enhancing the overall user interface and ensuring consistency with the new styling defined in app.css.

--- src/routes/ledgers/budgets/[budgetId]/transactions/new/+page.svelte
+++ src/routes/ledgers/budgets/[budgetId]/transactions/new/+page.svelte
@@ -8,25 +8,37 @@
 	{#snippet header()}
 		<h3 class="card-title">New Transaction</h3>
 	{/snippet}
-	<form method="post" use:enhance>
-		<label for="type">Type</label>
-		<select id="type" name="type">
-			{#each TRANSACTION_TYPES as type (type)}
-				<option value={type}>{type}</option>
-			{/each}
-		</select>
-		<label for="description">Description</label>
-		<input id="description" type="text" name="description" placeholder="Description" />
-		<label for="category">Category</label>
-		<select id="category" name="category">
-			{#each TRANSACTION_CATEGORIES as category (category)}
-				<option value={category}>{category}</option>
-			{/each}
-		</select>
-		<label for="amount">Amount</label>
-		<input id="amount" type="number" name="amount" placeholder="Amount" step="0.01" min="0" />
-		<label for="date">Date</label>
-		<input id="date" type="date" name="date" />
-		<button type="submit">Create</button>
+	<form method="post" use:enhance class="form">
+		<div class="form-group">
+			<label for="type">Type </label>
+			<select id="type" name="type">
+				{#each TRANSACTION_TYPES as type (type)}
+					<option value={type}>{type}</option>
+				{/each}
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="description">Description </label>
+			<textarea id="description" name="description" placeholder="Description"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="category">Category</label>
+			<select id="category" name="category">
+				{#each TRANSACTION_CATEGORIES as category (category)}
+					<option value={category}>{category}</option>
+				{/each}
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="amount">Amount</label>
+			<input id="amount" type="number" name="amount" placeholder="Amount" step="0.01" min="0" />
+		</div>
+		<div class="form-group">
+			<label for="date">Date</label>
+			<input id="date" type="date" name="date" />
+		</div>
+		<div class="form-actions">
+			<button type="submit" class="btn-primary">Create</button>
+		</div>
 	</form>
 </Card>
