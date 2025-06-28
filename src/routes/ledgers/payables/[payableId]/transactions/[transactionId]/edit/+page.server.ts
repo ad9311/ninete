b6ce226f83d84f7cdb09f0e1369a7cf857775461
Ángel Díaz <ad9311@proton.me>
2025-06-27 23:40:12 -0500

    Implement edit for payable transactions

---
+++ src/routes/ledgers/payables/[payableId]/transactions/[transactionId]/edit/+page.server.ts
@@ -0,0 +1,39 @@
+import { fail, redirect } from '@sveltejs/kit';
+import { updateTransaction } from '$lib/server/models/transactions';
+import type { Actions } from './$types';
+import { formatFormErrors, type TRANSACTION_CATEGORIES } from '$lib/shared';
+import type { ZodError } from 'zod';
+
+export const actions: Actions = {
+	default: async (event) => {
+		const formData = await event.request.formData();
+
+		const { payableId, transactionId } = event.params;
+
+		const description = formData.get('description') as string;
+		const amount = formData.get('amount') as string;
+		const category = formData.get('category') as Exclude<
+			(typeof TRANSACTION_CATEGORIES)[number],
+			'payable' | 'receivable'
+		>;
+		const date = new Date(formData.get('date') as string);
+		const isEstimated = formData.get('is_estimated');
+
+		const params = {
+			description,
+			amount,
+			category,
+			date,
+			isEstimated: isEstimated === 'on'
+		};
+
+		try {
+			await updateTransaction(Number(payableId), 'payable', Number(transactionId), params);
+		} catch (e) {
+			const errors = formatFormErrors(e as Error | ZodError);
+			return fail(400, { errors });
+		}
+
+		redirect(303, `/ledgers/payables/${payableId}`);
+	}
+};
---
+++ src/routes/ledgers/payables/[payableId]/transactions/[transactionId]/edit/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { type BreadcrumbItem } from '$lib/client';
+	import FormErrors from '$lib/components/form/FormErrors.svelte';
+	import Breadcrumb from '$lib/components/ui/Breadcrumb.svelte';
+	import Card from '$lib/components/ui/Card.svelte';
+	import Form from '$lib/components/transaction/Form.svelte';
+	import type { PageProps } from './$types';
+
+	const { data, form }: PageProps = $props();
+
+	const { transaction, payable } = data;
+	const breadcrumbItems: BreadcrumbItem[] = [
+		{ label: 'Home', href: '/' },
+		{ label: 'Payables', href: '/ledgers/payables' },
+		{ label: 'Payable', href: `/ledgers/payables/${payable.id}` },
+		{ label: 'Transaction', href: `/ledgers/payables/${payable.id}/transactions/${transaction.id}` },
+		{ label: 'Edit' }
+	];
+</script>
+
+<Breadcrumb items={breadcrumbItems} />
+<Card className="max-w-xl">
+	{#snippet header()}
+		<h3 class="card-title">Edit Transaction</h3>
+	{/snippet}
+	<FormErrors errors={form?.errors} />
+	<Form {transaction} />
+</Card>
