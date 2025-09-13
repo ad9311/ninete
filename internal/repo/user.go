094a91170f7b0ed2627225eee90c42e38b317d85
Ángel Díaz <ad9311@proton.me>
2025-09-13 15:29:05 -0500

    Remove nil init from err

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -40,7 +40,7 @@ RETURNING *`
 
 func (q *Queries) InsertUser(ctx context.Context, params InsertUserParams) (User, error) {
 	var u User
-	var err error = nil
+	var err error
 
 	q.wrapQuery(insertUser, func() {
 		row := q.db.QueryRowContext(
@@ -68,7 +68,7 @@ const selectUserByEmail = `SELECT * FROM users WHERE email = $1 LIMIT 1`
 
 func (q *Queries) SelectUserWhereEmail(ctx context.Context, email string) (User, error) {
 	var u User
-	var err error = nil
+	var err error
 
 	q.wrapQuery(selectUserByEmail, func() {
 		row := q.db.QueryRowContext(ctx, selectUserByEmail, email)
