52aca4713c7cfe9aee548216d15afc946a2c361e
Ángel Díaz <ad9311@proton.me>
2025-09-18 22:57:11 -0500

    Fix typo

--- internal/db/migrations/20250913160229_create_users_table.sql
+++ internal/db/migrations/20250913160229_create_users_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE "users" (
   "id" INTEGER PRIMARY KEY NOT NULL,
-  'username' TEXT NOT NULL UNIQUE,
+  "username" TEXT NOT NULL UNIQUE,
   "email" TEXT NOT NULL UNIQUE,
   "password_hash" BLOB NOT NULL,
   "created_at" INTEGER NOT NULL DEFAULT (strftime('%s','now')),
