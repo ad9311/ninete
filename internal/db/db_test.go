6dc4c208e9d519325997c56eb951ab5cd53389c6
Ángel Díaz <ad9311@proton.me>
2025-09-23 20:35:38 +0200

    Implement tests for db

---
+++ internal/db/db_test.go
@@ -0,0 +1,37 @@
+package db_test
+
+import (
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/ad9311/ninete/internal/db"
+	"github.com/ad9311/ninete/internal/prog"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOpen(t *testing.T) {
+	_, err := prog.Load()
+	require.Nil(t, err)
+
+	sqlDB, err := db.Open()
+	require.Nil(t, err)
+
+	cases := []struct {
+		name     string
+		testFunc func(*testing.T)
+	}{
+		{
+			"should_set_the_correct_max_connections",
+			func(t *testing.T) {
+				maxOpenConns := os.Getenv("MAX_OPEN_CONNS")
+				value := strconv.Itoa(sqlDB.Stats().MaxOpenConnections)
+				require.Equal(t, value, maxOpenConns)
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, tc.testFunc)
+	}
+}
