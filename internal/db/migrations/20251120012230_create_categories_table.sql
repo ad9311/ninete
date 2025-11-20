e9b1eabafa3275efdbbafb6685401cdab1f6590c
Ángel Díaz <ad9311@proton.me>
2025-11-19 22:22:47 -0500

    Add migrations for categories and expenses

---
+++ internal/db/migrations/20251120012230_create_categories_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS "categories" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+	"name" TEXT NOT NULL UNIQUE,
+	"uid" TEXT NOT NULL UNIQUE,
+	"created_at" INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+  "updated_at" INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+);
+
+PRAGMA user_version = 3;
+
+-- +goose Down
+DROP TABLE IF EXISTS "categories";
+
+PRAGMA user_version = 2;
---
+++ internal/db/migrations/20251120031225_create_expenses_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS "expenses" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+	"user_id" INTEGER NOT NULL REFERENCES "users"("id") ON DELETE CASCADE,
+	"category_id" INTEGER NOT NULL REFERENCES "categories"("id") ON DELETE CASCADE,
+	"description" TEXT NOT NULL,
+	"amount" REAL NOT NULL,
+  "date" DATE NOT NULL,
+  "created_at" INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+  "updated_at" INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+);
+
+PRAGMA user_version = 4;
+
+-- +goose Down
+DROP TABLE IF EXISTS "expenses";
+
+PRAGMA user_version = 3;
