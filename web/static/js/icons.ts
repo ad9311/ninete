25eafc1adead306184c263e55d7bec2b493d0336
Ángel Díaz <ad9311@proton.me>
2026-05-19 09:08:56 -0500

    Refactor UI

---
+++ web/static/js/icons.ts
@@ -0,0 +1,44 @@
+import {
+  createIcons,
+  Apple,
+  Calendar,
+  CalendarRange,
+  ChartColumn,
+  Download,
+  Eye,
+  Plus,
+  Repeat,
+  Salad,
+  Smile,
+  SquareArrowOutUpRight,
+  SquarePen,
+  Tag,
+  Target,
+  Trash2,
+  Utensils,
+  Wallet,
+} from "lucide";
+
+const icons = {
+  Apple,
+  Calendar,
+  CalendarRange,
+  ChartColumn,
+  Download,
+  Eye,
+  Plus,
+  Repeat,
+  Salad,
+  Smile,
+  SquareArrowOutUpRight,
+  SquarePen,
+  Tag,
+  Target,
+  Trash2,
+  Utensils,
+  Wallet,
+};
+
+export function initIcons(): void {
+  createIcons({ icons });
+}
